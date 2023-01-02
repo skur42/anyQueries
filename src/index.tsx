1540c4ffb0254eb0a279fb15ff896918a2015de9
Ankur Singh <ankur160128@gmail.com>
2023-01-02 16:57:31 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import dash from 'dash';
+import dash2 from 'dash2';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
